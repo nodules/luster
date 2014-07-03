d059f61fa241b9ce2c2e32cfef38c446c1255596
wayne <webstalk3r@gmail.com>
2014-07-03 17:42:26 +0400

    Log domain errors directly to stderr

--- bin/luster.js
+++ bin/luster.js
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
-var legacy = require('../lib/legacy'),
-    domain = require('domain').create();
+var domain = require('domain').create(),
+    path = require('path'),
+    configFilePath;
 
 domain.on('error', function(error) {
-    var fs = require('fs'),
-        path = require('path'),
-        dumpFilePath = path.resolve(
-            legacy.tmpdir(),
-            'luster-dump-' + process.pid + '-' +
-                (new Date()).toISOString().replace(/[^\dTZ]/g, '-'));
-
-    fs.writeFileSync(dumpFilePath,
-        (error instanceof Error) ?
+    process.stderr.write('Luster error [' + configFilePath + ']: ' +
+        (error instanceof Error ?
             error.message + '\n' + error.stack :
-            error);
+            error));
 
     process.exit(1);
 });
@@ -21,7 +15,6 @@ domain.on('error', function(error) {
 domain.run(function() {
     var /** @type {ClusterProcess} */
         luster = require('../lib/luster'),
-        path = require('path'),
         configFilePath = path.resolve(process.cwd(), process.argv[2] || 'luster.conf');
 
     luster.configure(require(configFilePath), true, path.dirname(configFilePath)).run();
