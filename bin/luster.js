0e5e4c1a5c10f19fdc9f9ab50b3995d75d7e406f
Phillip Kovalev <twilightfeel@gmail.com>
2014-10-03 20:42:04 +0400

    drop useless domain wrapper around luster configuration and launching

--- bin/luster.js
+++ bin/luster.js
@@ -1,21 +1,7 @@
 #!/usr/bin/env node
-var domain = require('domain').create(),
+var /** @type {ClusterProcess} */
+    luster = require('../lib/luster'),
     path = require('path'),
-    configFilePath;
+    configFilePath = path.resolve(process.cwd(), process.argv[2] || 'luster.conf');
 
-domain.on('error', function(error) {
-    process.stderr.write('Luster error [' + configFilePath + ']: ' +
-        (error instanceof Error ?
-            error.message + '\n' + error.stack :
-            error));
-
-    process.exit(1);
-});
-
-domain.run(function() {
-    var /** @type {ClusterProcess} */
-        luster = require('../lib/luster'),
-        configFilePath = path.resolve(process.cwd(), process.argv[2] || 'luster.conf');
-
-    luster.configure(require(configFilePath), true, path.dirname(configFilePath)).run();
-});
+luster.configure(require(configFilePath), true, path.dirname(configFilePath)).run();
