39e5256789d419f3a6e169659d3ce48ff7cc79f6
slava-b <slava-b@yandex-team.ru>
2017-02-14 17:18:18 +0300

    Added methods to start and clear force stop timeout `stopTimeout` parameter now uses `worker.process.kill()` to kill hung workers `stopTimeout` time now starts counting from `restart()`, `stop()` calls or `disconnect` event, whichever happens first
    Added functional tests for `stopTimeout` feature change
    Added comment for `_scheduleForceStop` checks.
    Moved functional tests to separate folder
    Moved --recursive option to `mocha.opts`
    Added rule for newline at file ends (and fixed two files)
    Removed `force_kill` example
    Added functional tests to `lint` checks

---
+++ test/func/fixtures/force_kill/master.js
@@ -0,0 +1,49 @@
+var proc = require('luster');
+
+proc
+    .configure({
+        app : 'worker.js',
+        workers : 1,
+        control : {
+            stopTimeout : 100
+        }
+    }, true, __dirname)
+    .run();
+
+if (proc.isMaster) {
+    proc.once('running', function() {
+        process.send('ready');
+    });
+    process.on('message', function(message) {
+        switch (message) {
+            case 'hang':
+                proc.remoteCallToAll('hang');
+                break;
+            case 'disconnect and hang':
+                proc.remoteCallToAll('disconnect and hang');
+                proc.once('worker disconnect', function() {
+                    process.send('disconnected');
+                });
+                break;
+            case 'wait worker':
+                proc.once('worker ready', function() {
+                    process.send('worker ready');
+                });
+                break;
+            case 'request':
+                proc.remoteCallToAllWithCallback({
+                    command : 'request',
+                    callback : function(worker, something, response) {
+                        process.send(response);
+                    }
+                });
+                break;
+            case 'restart':
+                proc.restart();
+                proc.once('restarted', function() {
+                    process.send('restarted');
+                });
+                break;
+        }
+    });
+}
---
+++ test/func/test/force_kill.js
@@ -0,0 +1,35 @@
+/* globals describe,it,before,after */
+'use strict';
+
+var LusterInstance = require('../helpers/luster_instance');
+
+describe('stopTimeout', function() {
+    var instance;
+
+    before(function() {
+        return LusterInstance
+            .run('../fixtures/force_kill/master.js')
+            .then(function (inst) {
+                instance = inst;
+            });
+    });
+
+    it('should kill infinite worker', function() {
+        return instance.sendWaitTimeout('hang', 10)
+            .then(function() {return instance.sendWaitAnswer('restart', 'restarted');})
+            .then(function() {return instance.sendWaitAnswer('request', 'response');});
+    });
+
+    it('should kill infinite worker that disconnected itself', function() {
+        return instance.sendWaitAnswer('disconnect and hang', 'disconnected')
+            .then(function() {return instance.sendWaitAnswer('wait worker', 'worker ready');})
+            .then(function() {return instance.sendWaitAnswer('request', 'response');});
+    });
+
+    after(function() {
+        if (instance) {
+            instance.kill();
+            instance = null;
+        }
+    });
+});
