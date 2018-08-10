41131ecfb420d90fd5d8896f5fe1a84c4eadf846
Ksenia V. Mamich <kvmamich@users.noreply.github.com>
2018-08-10 17:34:27 +0300

    Config path is next argument after luster filename

--- bin/luster.js
+++ bin/luster.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 const /** @type {ClusterProcess} */
     luster = require('../lib/luster'),
-    path = require('path'),
-    configFilePath = path.resolve(process.cwd(), process.argv[2] || 'luster.conf');
+    path = require('path');
+
+// config path is right after this script in process.argv
+const scriptArgvIndex = process.argv.findIndex(arg => arg === __filename || path.resolve(arg) === __filename);
+const configFilePath = path.resolve(process.cwd(), process.argv[scriptArgvIndex + 1] || 'luster.conf');
 
 luster.configure(require(configFilePath), true, path.dirname(configFilePath)).run();
